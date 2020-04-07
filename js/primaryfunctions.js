cf354da1b4579f53acc36dd643f5ca7adb288f91
AustinLeath <austinleath18@gmail.com>
2020-04-07 16:49:48 -0500

    changes

--- js/primaryfunctions.js
+++ js/primaryfunctions.js
@@ -86,14 +86,6 @@ function checkKeyPressed(e) {
   }
 }
 
-var h5 = $('h5[id^="cycletxt-"]').hide(),
-  i = 0;
-(function cycletxt() {
-  h5.eq(i).fadeIn(1500).delay(6500).fadeOut(1500, cycletxt);
-  i = ++i % h5.length;
-})();
-
-
 //preset method
 function checkmmrfill() {
   var finalmessage;
