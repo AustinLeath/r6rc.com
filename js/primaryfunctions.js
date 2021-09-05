b6a771fcf3443d39ef5f3b7e4aa1e75729b8044b
AustinLeath <austinleath18@gmail.com>
2021-09-04 22:00:10 -0500

    remove methodcustom and methodpreset and add them to primaryfunctions.js

--- js/primaryfunctions.js
+++ js/primaryfunctions.js
@@ -1,3 +1,144 @@
+// method preset
+function winorlose() {
+  var winorlose;
+
+  var a = document.getElementById("mmr").value;
+  var b = document.getElementById("elo").value;
+  var c = document.getElementById("goal").value;
+  var d = ((c - a) / b);
+  var e = Math.ceil(d);
+
+  if (e > 0) {
+      winorlose = "You need to <b>win</b> ";
+  } else if (e < 0) {
+      winorlose = "You need to <b>lose</b> ";
+  } else {
+      winorlose = "You <b>do not</b> need to win or lose ";
+  }
+  document.getElementById("winorlose").innerHTML = winorlose;
+}
+(function() {
+  function calculateRankGoal(mmr, elo, goal) {
+      mmr = parseFloat(mmr);
+      elo = parseFloat(elo);
+      goal = parseFloat(goal);
+
+      var a = ((goal - mmr) / elo);
+      var b = Math.ceil(a);
+      var c = Math.abs(b);
+      return c;
+  }
+
+  var preset = document.getElementById("preset");
+  if (preset) {
+      preset.onsubmit = function() {
+          document.getElementById("result").innerHTML = calculateRankGoal(this.mmr.value, this.elo.value, this.goal.value);
+          return false;
+      };
+  }
+}());
+function matchcount() {
+  var matchcount;
+
+  var a = document.getElementById("mmr").value;
+  var b = document.getElementById("elo").value;
+  var c = document.getElementById("goal").value;
+  var d = ((c - a) / b);
+  var e = Math.ceil(d);
+  var f = Math.abs(e);
+
+  if (f == 1) {
+      matchcount = " (&plusmn 1) match to reach your rank goal";
+  } else if (f > 1) {
+      matchcount = " (&plusmn 1) matches to reach your rank goal";
+  } else {
+      matchcount = " matches to reach your rank goal";
+  }
+document.getElementById("matchcount").innerHTML = matchcount;
+}
+function dontwinorlose() {
+var a = document.getElementById("winorlose").innerHTML;
+
+if (a == "You <b>do not</b> need to win or lose ") {
+  document.getElementById("result").style.display = "none";
+  document.getElementById("anydiv").innerHTML = "<b>any</b>";
+} else {
+  document.getElementById("result").style.display = "";
+  document.getElementById("anydiv").innerHTML = "";
+}
+}
+
+// method custom
+function winorlose1() {
+  var winorlose;
+
+  var a = document.getElementById("mmr1").value;
+  var b = document.getElementById("elo1").value;
+  var c = document.getElementById("goal1").value;
+  var d = ((c - a) / b);
+  var e = Math.ceil(d);
+
+  if (e > 0) {
+      winorlose = "You need to <b>win</b> ";
+  } else if (e < 0) {
+      winorlose = "You need to <b>lose</b> ";
+  } else {
+      winorlose = "You <b>do not</b> need to win or lose ";
+  }
+  document.getElementById("winorlose1").innerHTML = winorlose;
+}
+(function() {
+  function calculateRankGoal1(mmr1, elo1, goal1) {
+      mmr1 = parseFloat(mmr1);
+      elo1 = parseFloat(elo1);
+      goal1 = parseFloat(goal1);
+
+      var a = ((goal1 - mmr1) / elo1);
+      var b = Math.ceil(a);
+      var c = Math.abs(b);
+      return c;
+  }
+
+  var custom = document.getElementById("custom");
+  if (custom) {
+      custom.onsubmit = function() {
+          document.getElementById("result1").innerHTML = calculateRankGoal1(this.mmr1.value, this.elo1.value, this.goal1.value);
+          return false;
+      };
+  }
+}());
+function matchcount1() {
+  var matchcount;
+
+  var a = document.getElementById("mmr1").value;
+  var b = document.getElementById("elo1").value;
+  var c = document.getElementById("goal1").value;
+  var d = ((c - a) / b);
+  var e = Math.ceil(d);
+  var f = Math.abs(e);
+
+  if (f == 1) {
+      matchcount = " (&plusmn 1) match to reach your rank goal";
+  } else if (f > 1) {
+      matchcount = " (&plusmn 1) matches to reach your rank goal";
+  } else {
+      matchcount = " matches to reach your rank goal";
+  }
+  document.getElementById("matchcount1").innerHTML = matchcount;
+}
+
+function dontwinorlose1() {
+var a = document.getElementById("winorlose1").innerHTML;
+
+if (a == "You <b>do not</b> need to win or lose ") {
+  document.getElementById("result1").style.display = "none";
+  document.getElementById("anydiv1").innerHTML = "<b>any</b>";
+} else {
+  document.getElementById("result1").style.display = "";
+  document.getElementById("anydiv1").innerHTML = "";
+}
+}
+
 //the preset() and custom() functions provide method switch functionality
 function preset() {
   document.getElementById("preset").style.display = "block";
